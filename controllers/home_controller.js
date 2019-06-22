c98f2d19dd5cd6c3c93924e8ce5860fba8a7b203
djcruz93 <dhruv.jain93@gmail.com>
2019-06-22 16:15:23 +0530

    add views template for home and profile

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,5 @@
 module.exports.home = function (req, res){
-    return res.end('<h1>Welcome to home page.</h1>');
+    return res.render('home', {
+        title: "Home",
+    });
 };
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,5 @@
 module.exports.profile = function (req, res){
-    return res.end('<h1> Users Profile </h1>');
+    return res.render('profile', {
+        title: "Profile",
+    });
 };
