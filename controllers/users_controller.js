a42eb9b7ae902dc9f7a2f1c77324f9e12a3ab7b2
djcruz93 <dhruv.jain93@gmail.com>
2019-06-24 18:25:08 +0530

    create views, actions and routes for sign-in and sign-up

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,20 @@
-module.exports.profile = function (req, res){
+//render the profile page
+module.exports.profile = function(req, res){
     return res.render('profile', {
         title: "Profile",
+    });
+};
+
+//render the sign-in page
+module.exports.signIn = function(req, res){
+    return res.render('user_sign_in', {
+        title: "Codeial | Sign In"
+    });
+};
+
+//render the sign-up page
+module.exports.signUp = function(req, res){
+    return res.render('user_sign_up', {
+        title: "Codeial | Sign Up"
     });
 };
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,13 @@ const router = express.Router();
 
 const usersController = require('../controllers/users_controller');
 
+//Handle the request for profile i.e. creates a route profile
 router.get('/profile', usersController.profile);
 
+//Handle the request for sign-in i.e. creates a route sign-in
+router.get('/sign-in', usersController.signIn);
+
+//Handle the request for sign-up i.e. creates a route sign-up
+router.get('/sign-up', usersController.signUp);
 
 module.exports = router;
