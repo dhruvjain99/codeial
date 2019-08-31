298bb0908fb6c12e61e959ce1154dcc8f5dededf
djcruz93 <dhruv.jain93@gmail.com>
2019-08-31 13:14:45 +0530

    Setup the passport-jwt-strategy i.e. the config file(create jwt strategy)

---
+++ config/passport-jwt-strategy.js
@@ -0,0 +1,27 @@
+const passport = require('passport');
+const passportJWTStrategy = require('passport-jwt').Strategy;
+const jwtExtractor = require('passport-jwt').ExtractJwt;
+
+const User = require('../models/user');
+
+const options = {
+    jwtFromRequest: jwtExtractor.fromAuthHeaderAsBearerToken(),
+    secretOrKey: 'codeial'
+};
+
+passport.use(new passportJWTStrategy(options, function(jwtPayload, done){
+    User.findById(jwtPayload._id, function(err, user){
+        if(err){
+            console.log('Error in Token ', err);
+            return done(null, false);
+        }
+
+        if(user){
+            return done(null, user);
+        } else{
+            return done(null, false);
+        }
+    });
+}));
+
+module.exports = passport;
