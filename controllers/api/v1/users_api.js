bea9665c72e69f67c5f905c00fe0acd1e4d9f6c0
djcruz93 <dhruv.jain93@gmail.com>
2019-08-31 13:41:13 +0530

    Create an action to generate and return json web token

---
+++ controllers/api/v1/users_api.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+
+const User = require('../../../models/user');
+
+module.exports.createSession = async function(req, res){
+    try{
+        let user = User.findOne({email: req.body.email});
+        if(user && (user.password == req.body.password)){
+            return res.json(200, {
+                message: 'Signed In successfully, keep the token safe.',
+                data: {
+                    token: jwt.sign(user.toJSON(), 'codeial', {expiresIn: '600000'})
+                }
+            })
+        } else{
+            return res.json(422, {
+                message: 'Incorrect username or password.'
+            });
+        }
+    }catch(err){
+        console.log('****************** Error ', err);
+        return res.json(500, {
+            message: 'Internal Server Error'
+        });
+    }
+}
