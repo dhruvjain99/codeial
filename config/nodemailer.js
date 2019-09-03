12dfd6bda22c3e9f9dc2efe29f9b69fb795037fe
djcruz93 <dhruv.jain93@gmail.com>
2019-09-03 17:50:27 +0530

    Setup config file for nodemailer

---
+++ config/nodemailer.js
@@ -0,0 +1,35 @@
+const nodemailer = require('nodemailer');
+const path = require('path');
+const ejs = require('ejs');
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+        user: 'dhruv.jain93',
+        pass: ''
+    }
+});
+
+let renderTemplate = function(data, relativePath){
+    let mailHTML;
+    ejs.renderFile(
+        path.join(__dirname, '../views/mailer', relativePath),
+        data,
+        function(err, newTemplate){
+            if(err){
+                console.log('Error in rendering email template ::', err);
+                return;
+            }
+            mailHTML = newTemplate;
+            return mailHTML;
+        }
+    )
+}
+
+module.exports = {
+    transporter: transporter,
+    renderTemplate: renderTemplate
+}
