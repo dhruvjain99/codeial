920952939f4286eb7d200dd88f5e2d98bdff49d0
djcruz93 <dhruv.jain93@gmail.com>
2019-10-05 15:57:18 +0530

    Make chat_box using html and css

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const MongoStore = require('connect-mongo')(session);
 const sassMiddleware = require('node-sass-middleware');
 const flash = require('connect-flash');
 const customMware = require('./config/middleware');
-const port = 8000;
+const port = 3000;
 const app = express();
 
 
--- models/reset_password_token.js
+++ models/reset_password_token.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const resetPasswordTokenSchema = mongoose.Schema({
+const resetPasswordTokenSchema = new mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
